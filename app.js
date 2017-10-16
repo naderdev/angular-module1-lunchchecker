e3584065e09f805706b9a2a1cde17ac386ee6f07
naderscode <naderscode@gmail.com>
2017-10-16 18:33:43 -0400

    add conditional styling

--- app.js
+++ app.js
@@ -5,13 +5,13 @@
   .controller("LunchCheckController", LunchCheckController);
 
   LunchCheckController.$inject = ['$scope', '$filter'];
-  function LunchCheckController($scope, $filter){
-      $scope.food = "";
-      $scope.message = "";
-      $scope.lunchMessageStyle = {};
-      $scope.lunchTextBoxStyle = {};
+    function LunchCheckController($scope, $filter){
+        $scope.food = "";
+        $scope.message = "";
+        $scope.lunchMessageColor = {};
+        $scope.lunchTextBoxColor = {};
 
-    function removeEmpty(arr){
+    function removeEmptyString(arr){
       for(var i = 0; i < arr.length; i++){
         if(arr[i] === "") {
           arr.splice(i, 1);
@@ -20,25 +20,25 @@
       return arr;
     }
 
-    $scope.check = function(){
+    $scope.checkLunch = function(){
       $scope.lunch = $scope.food.split(",");
       $scope.lunch = $scope.lunch.map(Function.prototype.call, String.prototype.trim);
-      $scope.lunch = removeEmpty($scope.lunch);
+      $scope.lunch = removeEmptyString($scope.lunch);
 
           if ($scope.lunch.length === 0){
             $scope.message = "Please enter data first";
-            $scope.lunchMessageStyle.style = {"color":"red"};
-            $scope.lunchTextBoxStyle.style = {"border-color":"red"};
+            $scope.lunchMessageColor.style = {"color":"red"};
+            $scope.lunchTextBoxColor.style = {"border-color":"red"};
           }
           else if ($scope.lunch.length > 3){
             $scope.message = "Too much!";
-            $scope.lunchMessageStyle.style = {"color":"orange"};
-            $scope.lunchTextBoxStyle.style = {"border-color":"orange"};
+            $scope.lunchMessageColor.style = {"color":"orange"};
+            $scope.lunchTextBoxColor.style = {"border-color":"orange"};
           }
           else{
             $scope.message = "Enjoy!";
-            $scope.lunchMessageStyle.style = {"color":"green", "font-weight":"bold"};
-            $scope.lunchTextBoxStyle.style = {"border-color":"green"};
+            $scope.lunchMessageColor.style = {"color":"green", "font-weight":"bold"};
+            $scope.lunchTextBoxColor.style = {"border-color":"green"};
           }
       };
   }
